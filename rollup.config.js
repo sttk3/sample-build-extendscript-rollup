c67e16b66fffa86a03276ac091e8fe052859e864
sttk3 <>
2026-01-04 18:08:04 +0900

    コメントに詳細説明を記述

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,41 @@
+// Babelのプラグインをインポートする
 import babel from '@rollup/plugin-babel' ;
-import packageInfo from './package.json' assert { type: 'json' } ;
 
-// 生成されたコードへの後処理集
+// package.jsonの情報をオブジェクトとして取得する。Node.js v20あたりを想定。
+// もしここでエラーが出る場合は、その下のコードに変更
+import packageInfo from './package.json' assert { type: 'json' } ; // v18-21
+// import packageInfo from './package.json' with { type: 'json' } ; // v22-
+
+// 生成されたコードを後から置換する、したたか企画製処理集をインポートする。
+// 開発作業やExtendScript実行結果を改善させるものだが、必須ではない
 import {
   tripleQuoteReplace,
   consoleReplace,
   avoidMemoryLeak,
 } from './rollup-plugin-sttk3-replace.js' ;
 
+// 設定本体
 export default {
+  // メイン(始点・エントリーポイント)のJavaScript
   input: 'src/index.js',
+
+  // JavaScript書き出し設定
   output: {
+    // 書き出し先。package.jsonのnameを流用する設定にしてある
     file: `dist/${packageInfo.name}.jsx`,
+
+    // 単体で実行可能なJavaScript (即時関数)に変換する設定にしてある。
+    // (function() {...})(); のような形式で出力される
     format: 'iife',
+
+    // 本番用書き出しではセキュリティを考慮してfalseにするのが一般的
     sourcemap: false,
 
-    // jsxファイルの先頭につけるコメント
+    /*
+       できるjsxファイルの先頭につけるコメント。
+       targetengineなどExtendScriptのPreprocessor directiveは
+       ソースコードから消えるので、指定するならここで書く必要がある
+    */
     banner: `/**
   * @file ファイル説明
   * @version ${packageInfo.version}
@@ -28,17 +48,26 @@ export default {
 `
   },
   
+  // プラグイン処理設定
   plugins: [
-    // 生成されたコードへの後処理
+    // rollup-plugin-sttk3-replace.jsに由来する、生成されたコードへの後処理。
+    // このプラグイン処理を実行する
     tripleQuoteReplace(),
     consoleReplace(),
     avoidMemoryLeak(),
 
+    // Babelの設定
     babel({
       babelHelpers: 'bundled',
+
+      // 同梱するJavaScriptパターン
       extensions: ['.js'],
       include: ['src/**/*'],
+
+      // 同梱しないJavaScriptパターン
       exclude: 'node_modules/**',
+
+      // 変換プリセットをInternet Explorer 8にすることで、間接的にECMAScriptバージョン(ES3)を指定する
       presets: [
         [
           '@babel/preset-env',
